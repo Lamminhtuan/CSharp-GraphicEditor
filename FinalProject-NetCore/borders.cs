594ee229451dd098b3995dd3bf40185ddd3e9a2d
Lamminhtuan <tuanlam300@gmail.com>
2023-01-11 20:50:30 +0700

    add some borders - done

--- FinalProject-NetCore/borders.cs
+++ FinalProject-NetCore/borders.cs
@@ -15,10 +15,14 @@ namespace FinalProject_NetCore
             borderurls = new List<string>();
             borderurls.Add("/images/border1.png");
             borderurls.Add("/images/border2.png");
-
             borderurls.Add("/images/border3.png");
             borderurls.Add("/images/border4.png");
             borderurls.Add("/images/border5.png");
+            borderurls.Add("/images/border6.png");
+            borderurls.Add("/images/border7.png");
+            borderurls.Add("/images/border8.png");
+            borderurls.Add("/images/border9.png");
+            borderurls.Add("/images/border10.png");
         }
     }
 }
